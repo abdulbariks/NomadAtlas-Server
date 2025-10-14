538595090e078dd42d668c6efded771c41ec8741
questcoderull <questcoderull@gmail.com>
2025-10-14 11:32:20 +0600

    Added more fields in citySchema for better result

--- models/cityForCostCalculatorModel.js
+++ models/cityForCostCalculatorModel.js
@@ -5,6 +5,13 @@ const citySchema = new mongoose.Schema({
   name: { type: String, required: true },
   livingCost: { type: Number, required: true },
   luxuryScore: { type: Number, required: true },
+
+  // Optional fields
+  internetSpeed: { type: Number }, // 80 means 80 Mbps
+  beachAccess: { type: Boolean }, // true or false
+  weather: { type: String }, // "tropical", "moderate", "cold"
+  lifestyle: { type: String }, // "tech", "nature", "cultural", "nightlife"
+  safetyScore: { type: Number }, // 85 means 85/100
 });
 
 // Country Schema
